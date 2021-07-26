f8c26504b88207809c0ba0dcfda79ecc73a27586
MBSN91 <82053276+MBSN91@users.noreply.github.com>
2021-07-26 23:51:33 +0200

    Add files via upload

---
+++ counterscript.js
@@ -0,0 +1,128 @@
+var waypoint = new Waypoint({
+    element: document.getElementById('stoffen'),
+    handler: function(direction) {
+      const options = {
+    duration: 5,
+  };
+  let demo = new CountUp('stoffen', 7068, options);
+  if (!demo.error) {
+    demo.start();
+  } else {
+    console.error(demo.error);
+  }
+    },
+    offset: '100%'
+  })
+
+  var waypoint = new Waypoint({
+    element: document.getElementById('voering'),
+    handler: function(direction) {
+      const options = {
+    duration: 5,
+  };
+  let demo = new CountUp('voering', 7068, options);
+  if (!demo.error) {
+    demo.start();
+  } else {
+    console.error(demo.error);
+  }
+    },
+    offset: '100%'
+  })  
+
+  var waypoint = new Waypoint({
+    element: document.getElementById('knoop'),
+    handler: function(direction) {
+      const options = {
+    duration: 5,
+  };
+  let demo = new CountUp('knoop', 7068, options);
+  if (!demo.error) {
+    demo.start();
+  } else {
+    console.error(demo.error);
+  }
+    },
+    offset: '100%'
+  })
+  
+  var waypoint = new Waypoint({
+    element: document.getElementById('kraag'),
+    handler: function(direction) {
+      const options = {
+    duration: 5,
+  };
+  let demo = new CountUp('kraag', 7068, options);
+  if (!demo.error) {
+    demo.start();
+  } else {
+    console.error(demo.error);
+  }
+    },
+    offset: '100%'
+  })
+  
+  var waypoint = new Waypoint({
+    element: document.getElementById('garen'),
+    handler: function(direction) {
+      const options = {
+    duration: 5,
+  };
+  let demo = new CountUp('garen', 7068, options);
+  if (!demo.error) {
+    demo.start();
+  } else {
+    console.error(demo.error);
+  }
+    },
+    offset: '100%'
+  })
+  
+  var waypoint = new Waypoint({
+    element: document.getElementById('zakken'),
+    handler: function(direction) {
+      const options = {
+    duration: 5,
+  };
+  let demo = new CountUp('zakken', 7068, options);
+  if (!demo.error) {
+    demo.start();
+  } else {
+    console.error(demo.error);
+  }
+    },
+    offset: '100%'
+  })
+
+  var waypoint = new Waypoint({
+    element: document.getElementById('broek'),
+    handler: function(direction) {
+      const options = {
+    duration: 5,
+  };
+  let demo = new CountUp('broek', 7068, options);
+  if (!demo.error) {
+    demo.start();
+  } else {
+    console.error(demo.error);
+  }
+    },
+    offset: '100%'
+  })
+
+  var waypoint = new Waypoint({
+    element: document.getElementById('gilet'),
+    handler: function(direction) {
+      const options = {
+    duration: 5,
+  };
+  let demo = new CountUp('gilet', 7068, options);
+  if (!demo.error) {
+    demo.start();
+  } else {
+    console.error(demo.error);
+  }
+    },
+    offset: '100%'
+  })
+  
